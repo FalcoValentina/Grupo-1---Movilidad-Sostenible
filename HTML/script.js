f2982607a51eae35923a25ac834e03e664ef7b95
micasolcariaga <micaelacariaga01@gmail.com>
2024-08-29 07:39:22 -0300

    add new changes save all changes

--- HTML/script.js
+++ HTML/script.js
@@ -1,18 +1,20 @@
-
-let Nombre = prompt ("Por favor")
-
-let Lugar = prompt ("¿Tu lugar de residencia es en CABA?")
-
-if (Lugar.toLowerCase() === "sí" || Lugar.toLowerCase() === "si") {
-    alert("¡Qué bueno que vivas en CABA!");
-} else if (Lugar.toLowerCase() === "no") {
-    alert("Entendido, no vives en CABA.");
-} else {
-    alert("Respuesta no válida. Por favor, responde con 'Sí' o 'No'.");
+async (params) => {
+    
+    let Nombre = prompt ("Por favor")
+    
+    let Lugar = prompt ("¿Tu lugar de residencia es en CABA?")
+    
+    if (Lugar.toLowerCase() === "sí" || Lugar.toLowerCase() === "si") {
+        alert("¡Qué bueno que vivas en CABA!");
+    } else if (Lugar.toLowerCase() === "no") {
+        alert("Entendido, no vives en CABA.");
+    } else {
+        alert("Respuesta no válida. Por favor, responde con 'Sí' o 'No'.");
+    }
+    
+    let mail = prompt("Para mantenerte informado, déjanos tu mail");
+    
+    alert(`Muchas gracias, ${Nombre}`);
 }
-
-let mail = prompt("Para mantenerte informado, déjanos tu mail");
-
-alert(`Muchas gracias, ${Nombre}`);
 
 
